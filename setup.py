5c8357deb4d971a01115fd904599f497b956865c
Edward Jose <edward@email.com>
2014-10-23 11:38:55 +0800

    changed setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='skc_g12',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
+      author='Edward Jose',
       author_email='example@example.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask>=0.7.2', 'MarkupSafe'],
+      install_requires=['Flask>=0.9', 'MarkupSafe'],
      )
